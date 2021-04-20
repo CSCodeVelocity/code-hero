d71467a4bdbbd837f2f2badd3be9d69aca217bb2
Carthanial <ccarney51@gmail.com>
2021-04-20 15:38:54 -0400

    checking out to pull db merge

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { authPage } from './pages/authPage.jsx';
+import { gamePage } from './pages/gamePage.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  return <div>App Rendering</div>;
+  return (
+    <div classname="App">
+      <h1>Protected React Router</h1>
+    </div>
+  );
 };
 
 export default App;
