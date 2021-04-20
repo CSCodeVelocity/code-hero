b547571e7a2381e2a056f838cb6013eee8f20955
johnli <jli159@binghamton.edu>
2021-04-20 18:51:57 -0400

    updated server.js for socket.io setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-// const server = require('http').createServer(app);
-// const io = require('socket.io')(server);
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 const path = require('path');
 const PORT = 3000;
 
@@ -10,6 +11,10 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
