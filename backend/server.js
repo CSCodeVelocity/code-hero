69e93e6e9e505c6b732c137840b60d12601806e5
Nathan Richardson <nathan.richardson94@gmail.com>
2021-04-20 16:06:57 -0500

    get user route implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
+const userRouter = require('./routes/users');
 
 const app = express();
-const path = require('path');
 const PORT = 3000;
 
 // handle incoming json
@@ -10,26 +11,25 @@ app.use(express.json());
 // serve bundle
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
+// serve html
+app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, '../index.html')));
 
+console.log('test');
 // api routes
-//app.use('/users', userRouter)
+app.use('/users', userRouter);
 
 // 404 handler
-app.use((req, res) => res.status(404).send('This Page does not exist')
+app.use((req, res) => res.status(404).send('This Page does not exist'));
 
 // Global error handler
-app.use(err, req, res, next) => {
+app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
     message: { err: 'An error occurred' },
   };
   const errorObj = Object.assign({}, defaultErr, err)
-  return res.status(errorObj.status).json(errorObj.message)
-}
-
+  return res.status(errorObj.status).json(errorObj.log);
+});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
