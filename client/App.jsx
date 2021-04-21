cbfd624a9c45352ed6f5a7b97895985d7db88b95
Carthanial <ccarney51@gmail.com>
2021-04-20 20:07:03 -0400

    working on fetch in authForm

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { authPage } from './pages/authPage.jsx';
 import { gamePage } from './pages/gamePage.jsx';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
--- client/components/authForm.jsx
+++ client/components/authForm.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
-import GamePage from '../pages/gamePage.jsx';
 
 export default function authForm(props) {
   console.log(props);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // const username = document.querySelector('#username').value;
+    // const password = document.querySelector('#password').value;
+
+    // fetch('/auth/login', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({
+    //     username: username,
+    //     password: password,
+    //   }),
+    // }).then(res => res.json())
+    //     .then(data => {
+    //         const { username } = res.body;
+    //         const { user_id } = res.body;
+
+    // })
+
+    props.history.push('/game');
+  };
+
+  const handleSignUp = () => {};
+
   return (
     <div className="authForm">
-      <input type="text" placeholder="Username"></input>
+      <input id="username" type="text" placeholder="Username"></input>
       <br></br>
-      <input type="text" placeholder="Password"></input>
+      <input id="password" type="text" placeholder="Password"></input>
       <br></br>
-      <button
-        className="authButton"
-        onClick={() => {
-          props.history.push('/game');
-        }}
-      >
-        AuthButton
+      <button id="logIn" className="authButton" onClick={handleLogin}>
+        Log In
+      </button>
+      <button id="signUp" className="authButton" onClick={handleSignUp}>
+        Sign Up
       </button>
     </div>
   );
