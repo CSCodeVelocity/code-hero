6110e4e4bc14d2dc51fc7b21033c9f117bcb4904
Carthanial <ccarney51@gmail.com>
2021-04-21 08:26:43 -0400

    Added handleLogin function to authForm.jsx 	-Contains basic functionality to check for username and password and route to gamePage if successful and stay at login if unsuccessful

--- client/components/authForm.jsx
+++ client/components/authForm.jsx
@@ -6,24 +6,29 @@ export default function authForm(props) {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    // const username = document.querySelector('#username').value;
-    // const password = document.querySelector('#password').value;
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#pass').value;
 
-    // fetch('/auth/login', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({
-    //     username: username,
-    //     password: password,
-    //   }),
-    // }).then(res => res.json())
-    //     .then(data => {
-    //         const { username } = res.body;
-    //         const { user_id } = res.body;
+    if (!username || !password) return; // Need to add message to user for empty fields
 
-    // })
-
-    props.history.push('/game');
+    fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('RESPONSE: ', res);
+        const { username, userId, success } = res;
+        // success === false ? do some state stuff and redirect back to login : route/redirect to /game with username and userId (also do some state stuffs)
+        if (!success) return;
+        else {
+          props.history.push('/game');
+        }
+      });
   };
 
   const handleSignUp = () => {};
@@ -32,7 +37,7 @@ export default function authForm(props) {
     <div className="authForm">
       <input id="username" type="text" placeholder="Username"></input>
       <br></br>
-      <input id="password" type="text" placeholder="Password"></input>
+      <input id="pass" type="password" placeholder="Password"></input>
       <br></br>
       <button id="logIn" className="authButton" onClick={handleLogin}>
         Log In
