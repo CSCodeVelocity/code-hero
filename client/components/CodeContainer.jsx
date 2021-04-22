8bd0e28963cc50b191ff170311b3bf5f53165874
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2021-04-22 03:30:14 -0400

    Update CodeContainer state logic
      Create updated logic to pass down into the input and block
      components.
    
      Ensure all action types are up to date.

--- client/components/CodeContainer.jsx
+++ client/components/CodeContainer.jsx
@@ -1,20 +1,45 @@
-import React, { useEffect, useReducer } from 'react';
-import useReducerThunk from '../state/customHooks.js';
+import React, { useEffect, useReducer, useCallback } from 'react';
+import {
+  CODEINPUT_INPUT_CODE,
+  CODEBLOCK_SUCCEED,
+  CODEBLOCK_WRITE_TO_BOX,
+} from '../state/actions.js';
+import { useReducerThunk } from '../state/customHooks.js';
 import { initialCodeState, codeReducer } from '../state/reducers.js';
 import CodeBlock from './CodeBlock.jsx';
 import CodeInput from './CodeInput.jsx';
 
 const CodeContainer = () => {
   const [state, dispatch] = useReducerThunk(codeReducer, initialCodeState);
-  useEffect(async () => {
-    const response = await fetch('../../data/codeblock.json');
-    console.log(response);
-  });
+
+  const writeToCodeState = action => {
+    dispatch(action);
+  };
+
+  const fetchCodeBlock = path => {
+    dispatch(async () => {
+      const response = await fetch(path);
+      const data = await response.json();
+      console.log('data from server', data);
+      dispatch({
+        type: CODEBLOCK_SUCCEED,
+        payload: { codeBlock: data.codeBlock },
+      });
+    });
+  };
+
   return (
     <div>
-      <span>Test stuff</span>
-      <CodeBlock code={} />
-      <CodeInput />
+      <CodeBlock
+        codeBlock={state.codeBlock}
+        fetchCodeBlock={fetchCodeBlock}
+        writeToCodeState={writeToCodeState}
+      />
+      <CodeInput
+        state={state}
+        codeBlock={state.codeBlock}
+        writeToCodeState={writeToCodeState}
+      />
     </div>
   );
 };
